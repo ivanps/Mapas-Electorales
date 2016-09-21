585c2722380ac4647f4a7d9571052aa938eeabc3
Iván Pacheco <ivanpsmx@msn.com>
2016-09-21 01:22:51 -0500

    Update ui.R

--- ggmap_partidos/ui.R
+++ ggmap_partidos/ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
     sidebarPanel(
       selectInput("part", "Partido:", c("TODOS", "PAN_PRD", "PRI_PVEM_PANAL", "PT", "MORENA",
                                         "PES"), selected = "TODOS"),
-      sliderInput("pciento", "% Preferencia", min=10, max=100, value=20),
+      sliderInput("pciento", "% Preferencia", min=0, max=100, value=20),
       sliderInput("magnif", "Zoom:", min=0, max=15, value=7),
       sliderInput("loclon", "Longitud", min=-105, max=-101, value = -102.5832, step = .01),
       sliderInput("loclat", "Latitud", min=21, max=26, value = 22.7709, step = .01),
